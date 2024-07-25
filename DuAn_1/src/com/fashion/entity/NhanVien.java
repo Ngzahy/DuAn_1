128adf337d298e24a972dbfd56d097c45ab4bc09
nghuyy <155972334+Ngzahy@users.noreply.github.com>
2024-07-25 14:47:19 +0700

    Add files via upload

---
+++ DuAn_1/src/com/fashion/entity/NhanVien.java
@@ -0,0 +1,75 @@
+
+package com.fashion.entity;
+
+public class NhanVien {
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    @Override
+    public String toString() {
+        return this.hoTen;
+    }
+
+    
+    public boolean isVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(boolean vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+    private String maNV;
+    private String matKhau;
+    private String hoTen;
+    private boolean vaiTro = false;
+    private boolean gioiTinh = false;
+    private String email;
+    private String sdt;
+    
+}
